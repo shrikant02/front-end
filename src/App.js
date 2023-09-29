3621b87a3122f91da42e78d85b850ac6ebd0081a
shrikant02 <shrikantnavghare2001@gmail.com>
2023-09-29 16:05:33 +0530

    project template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const baseUrl = "http://localhost:8080/";
+
+  const fetchUserData = () => {
+    axios.get(baseUrl + "api/v1/data").then((response) => {
+      setUsers(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  console.log(users);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {users.length > 0 && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.endYear}>{user.sector}</li>
+          ))}
+          <li>{user.intensity}</li>
+        </ul>
+      )}
     </div>
   );
 }
